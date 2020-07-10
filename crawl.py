76beaa3ed06cc014cbf5bcb4d670890cd8aa7b7a
TerrifyGary <yoyoman1415926@gmail.com>
2020-07-10 16:32:04 +0800

    changing crawl.py data type

--- crawl.py
+++ crawl.py
@@ -1,14 +1,16 @@
 import sqlite3
-import pandas as pd
-import jieba
 from collections import Counter
+
+import jieba
+import pandas as pd
+
 # Create your connection.
-conn = sqlite3.connect('./Shopee_R-F_Identification/Data.db')
-jieba.set_dictionary('./Shopee_R-F_Identification/dict.txt.big.txt')
+conn = sqlite3.connect('./Data.db')
+jieba.set_dictionary('./dict.txt.big.txt')
 def top_words(star, num):
     df = pd.read_sql_query("SELECT * FROM rating", conn)
     df = pd.read_sql_query("SELECT * FROM rating WHERE star == (?)", conn, params=(star,))
-    stopwords = ['的']
+    stopwords = ['的','n','我','有','了']
     comment_ = df['comment'].to_string()
     comment_ = ''.join(e for e in comment_ if e.isalpha())
     comment_ = ''.join(e for e in comment_ if e not in stopwords)
@@ -19,13 +21,14 @@ def top_words(star, num):
     for ele in words: 
         if ele.strip('n'): 
             res.append(ele)     
-    return Counter(words).most_common(num)
+    # return Counter(words).most_common(num)
+    return res,Counter(res)
 
 
 
-num = 10
+num = 20
 for x in range(5):
-    print('Number of star:',x+1,'Top 10 comments: ',top_words(x+1, num))
+    print('Number of star:',x+1,'Top 10 comments: ',top_words(x+1, num)[0])
     
-conn.commit()
-conn.close()
+# conn.commit()
+# conn.close()
