adc2d1212f00833dd1d15e0e5447ffe41041a19d
LaoShihChieh <410621207@gms.ndhu.edu.tw>
2020-07-28 06:01:34 +0800

    remove isalpha() limit

--- Shopee_Comments_Similarity.py
+++ Shopee_Comments_Similarity.py
@@ -1,30 +1,29 @@
 import sqlite3
 import jieba
 from collections import Counter
+import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 #https://towardsdatascience.com/overview-of-text-similarity-metrics-3397c4601f50
-jieba.set_dictionary('./Shopee_R-F_Identification/dict.txt.big.txt')
+jieba.set_dictionary('./dict.txt.big.txt')
 
 def get_star_words(star, data):
-    conn = sqlite3.connect('./Shopee_R-F_Identification/'+data)
+    conn = sqlite3.connect('./'+data)
     df = pd.read_sql_query("SELECT * FROM rating", conn)
     df = pd.read_sql_query("SELECT * FROM rating WHERE star == (?)", conn, params=(star,))
     stopwords = ['的']
     comment_ = df['comment']
     comment_ = ''.join(e for e in comment_ if e not in stopwords)
-    comment_ = ''.join(e for e in comment_ if e.isalpha())
     words = jieba.lcut(comment_)
     comment_ = ' '.join(e for e in words)
     return comment_
 
 def get_all_words(data):
-    conn = sqlite3.connect('./Shopee_R-F_Identification/'+data)
+    conn = sqlite3.connect('./'+data)
     df = pd.read_sql_query("SELECT * FROM rating", conn)
     stopwords = ['的']
     comment_ = df['comment']
     comment_ = ''.join(e for e in comment_ if e not in stopwords)
-    comment_ = ''.join(e for e in comment_ if e.isalpha())
     words = jieba.lcut(comment_)
     comment_ = ' '.join(e for e in words)
     return comment_
