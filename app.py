674f7d1e06c74fb267bf698a4be433f310ccc6e5
TerrifyGary <yoyoman1415926@gmail.com>
2020-08-10 14:18:22 +0800

    修改一下了 GUI 還有 大致上完成惹 Delta Price der功能 嘿

--- app.py
+++ app.py
@@ -1,37 +1,47 @@
 from flask import Flask, render_template, request
+import Shopee_Project_Recognizing as sp
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('app.html',static_url_path='/static')
+    return render_template('app.html', static_url_path='/static')
 
 
-@app.route('/send', methods=['POST'])
+@app.route('/send', methods=['POST','GET'])
 def send(sum=sum):
     if request.method == 'POST':
-        num1 = request.form['num1']
-        num2 = request.form['num2']
+        product_url = request.form['Product_URL']
+        product_name = request.form['Product_Name']
         operation = request.form['operation']
+        sum = 0
 
-        if operation == 'add':
-            sum = float(num1) + float(num2)
+        if operation == 'fake_words':
+            sum = float(product_url) + float(product_name)
+
             return render_template('app.html', sum=sum)
 
-        elif operation == 'subtract':
-            sum = float(num1) - float(num2)
+        elif operation == 'shipping_time':
+            sum = float(product_url) - float(product_name)
+
             return render_template('app.html', sum=sum)
 
-        elif operation == 'multiply':
-            sum = float(num1) * float(num2)
+        elif operation == 'rating_stars':
+            sum = float(product_url) * float(product_name)
+
             return render_template('app.html', sum=sum)
 
-        elif operation == 'divide':
-            sum = float(num1) / float(num2)
+        elif operation == 'delta_price':
+            # sum = float(product_url) / float(product_name)
+            sum = sp.get_mall_price(
+                f"https://shopee.tw/mall/search?keyword={product_name}")
+
             return render_template('app.html', sum=sum)
         else:
             return render_template('app.html')
+
+#⚠️⚠️⚠️------ Here Starts All Functions -------⚠️⚠️⚠️#
 
 
 if __name__ == ' __main__':
