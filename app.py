bca9e0d7c2412906d51ac896e5a62b0c73b5370f
TerrifyGary <yoyoman1415926@gmail.com>
2020-08-15 01:27:24 +0800

    ㄍㄊㄇㄉ瞎爆了瞎皮！！！到底沒事尬麻把user name 隱藏呀 沃艸勒！！！

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
 import Shopee_Project_Recognizing as sp
+from bs4 import BeautifulSoup
+import requests
+import time
+import random
+import json
+import sqlite3
+import timeit
+import time
 
 app = Flask(__name__)
 
@@ -15,16 +23,26 @@ def send(sum=sum):
         product_url = request.form['Product_URL']
         product_name = request.form['Product_Name']
         operation = request.form['operation']
+
+        seller_comment = []
+        num_of_comment = []
+
+        price, text = sp.get_web_info(product_url)
+
+        for x in range(1,6,1):
+            sp.scraping_comments(text,x)
+        
         sum = 0
 
-        if operation == 'fake_words':
-            sum = float(product_url) + float(product_name)
 
+        if operation == 'fake_words':
+
+            sum = int(sp.couting_fake(seller_comment))
             return render_template('app.html', sum=sum)
 
         elif operation == 'shipping_time':
-            sum = float(product_url) - float(product_name)
 
+            sum = float(product_url) - float(product_name)
             return render_template('app.html', sum=sum)
 
         elif operation == 'rating_stars':
@@ -35,7 +53,8 @@ def send(sum=sum):
         elif operation == 'delta_price':
             # sum = float(product_url) / float(product_name)
             sum = sp.get_mall_price(
-                f"https://shopee.tw/mall/search?keyword={product_name}")
+                f"https://shopee.tw/mall/search?keyword={product_name}"
+                ) - price
 
             return render_template('app.html', sum=sum)
         else:
