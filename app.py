6990467acf659ba86c2923595333b8a0b106fa10
TerrifyGary <yoyoman1415926@gmail.com>
2020-08-18 16:45:46 +0800

    finishing the output of html

--- app.py
+++ app.py
@@ -27,40 +27,45 @@ def send(sum=sum):
         seller_comment = []
         # num_of_comment = []
 
-        price, text = sp.get_web_info(product_url)
+        price, sellername = sp.get_web_info(product_url)
 
         for x in range(1,6,1):
-            sp.scraping_comments(text,x)
+            sp.scraping_comments(sellername,x)
         
         sum = 0
 
 
         if operation == 'fake_words':
-
-            sum = int(sp.couting_fake(seller_comment))
+            
+            sum = operation + ' = ' + str(sp.couting_fake(seller_comment)) + ' times.'
             return render_template('app.html', sum=sum)
 
         elif operation == 'response_rate':
 
-            sum = float(product_url) - float(product_name)
+            sum = operation + ' = ' + str(sp.response_rate(sellername)*100) + '%'
+            return render_template('app.html', sum=sum)
+        
+        elif operation == 'rating_bad':
+
+            sum = operation + ' = ' + str(sp.rating_bad(sellername)) + ' times.'
             return render_template('app.html', sum=sum)
 
         elif operation == 'rating_stars':
-            sum = float(product_url) * float(product_name)
 
+            sum = operation + ' = ' + str(sp.rating_star(sellername)) + ' stars.'
             return render_template('app.html', sum=sum)
 
         elif operation == 'delta_price':
             # sum = float(product_url) / float(product_name)
-            sum = sp.get_mall_price(
+            sum = operation + ' = ' +str(int(sp.get_mall_price(
                 f"https://shopee.tw/mall/search?keyword={product_name}"
-                ) - price
+                ) - price))+' $NTD'
 
             return render_template('app.html', sum=sum)
         else:
             return render_template('app.html')
 
-#⚠️⚠️⚠️------ Here Starts All Functions -------⚠️⚠️⚠️#
+#⚠️⚠️⚠️------ Here Starts All Functions -------⚠️⚠️⚠️#  
 
 
 if __name__ == ' __main__':
