58b1fd3a74e89ccb4787c8bd77a331a2a4ceffbf
TerrifyGary <yoyoman1415926@gmail.com>
2020-08-31 12:09:16 +0800

    機掰啦幹 哈哈

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ import timeit
 import time
 
 app = Flask(__name__)
-def Scroring(delta_price,response_rate,rating_bad,fake_words):
 
 
 @app.route('/')
 def main():
     return render_template('app.html', static_url_path='/static')
+
 
 @app.route('/send', methods=['POST','GET'])
 def send(sum=sum):
@@ -28,42 +28,60 @@ def send(sum=sum):
         # num_of_comment = []
 
         price, sellername = sp.get_web_info(product_url)
+        fake_words = sp.couting_fake(seller_comment)
+        response_rate = sp.response_rate(sellername)*100
+        rating_bad = sp.rating_bad(sellername)
+        rating_stars = sp.rating_star(sellername)
+        delta_price = int(sp.get_mall_price(f"https://shopee.tw/mall/search?keyword={product_name}") - price)
 
+        score = int(sp.scoring(delta_price,fake_words,rating_bad,rating_stars)%1000)
         for x in range(1,6,1):
             sp.scraping_comments(sellername,x)
-        
-        sum = 0
-
 
         if operation == 'fake_words':
             
-            sum = operation + ' = ' + str(sp.couting_fake(seller_comment)) + ' times.'
+            sum = operation + ' = ' + str(fake_words) + ' times.' + '    Final Score is    ' + str(score)
             return render_template('app.html', sum=sum)
 
         elif operation == 'response_rate':
 
-            sum = operation + ' = ' + str(sp.response_rate(sellername)*100) + '%'
+            sum = operation + ' = ' + str(response_rate) + '%' + '    Final Score is    ' + str(score)
             return render_template('app.html', sum=sum)
         
         elif operation == 'rating_bad':
 
-            sum = operation + ' = ' + str(sp.rating_bad(sellername)) + ' times.'
+            sum = operation + ' = ' + str(rating_bad) + ' times.' + '    Final Score is    ' + str(score)
             return render_template('app.html', sum=sum)
 
         elif operation == 'rating_stars':
 
-            sum = operation + ' = ' + str(sp.rating_star(sellername)) + ' stars.'
+            sum = operation + ' = ' + str(rating_stars) + ' stars.' + '    Final Score is    ' + str(score)
             return render_template('app.html', sum=sum)
 
         elif operation == 'delta_price':
             # sum = float(product_url) / float(product_name)
-            sum = operation + ' = ' +str(int(sp.get_mall_price(
-                f"https://shopee.tw/mall/search?keyword={product_name}"
-                ) - price))+' $NTD'
+            sum = operation + ' = ' +str(delta_price)+' $NTD' + '    Final Score is    ' + str(score)
 
             return render_template('app.html', sum=sum)
         else:
             return render_template('app.html')
+
+
+# @app.route('/send', methods=['POST','GET'])
+# def send(score):
+#     if request.method == 'POST':
+#         product_url = request.form['Product_URL']
+#         product_name = request.form['Product_Name']
+#         operation = request.form['operation']
+
+#         seller_comment = []
+#         # num_of_comment = []
+
+#         price, sellername = sp.get_web_info(product_url)
+
+#         mall_price = int(sp.get_mall_price(f'https://shopee.tw/mall/search?keyword={product_name}') 
+
+#         return render_template('app.html',score = 0)
 
 #⚠️⚠️⚠️------ Here Starts All Functions -------⚠️⚠️⚠️#  
 
